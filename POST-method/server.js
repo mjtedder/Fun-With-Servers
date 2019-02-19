9db0b90d65fd049b5a2ddf357efa61a6fa607e70
mjtedder <mjtedder@gmail.com>
2019-02-19 11:27:22 -0700

    res.writeHead is not a function
    In my switch statement I was passing in too many arguments, so I think
    this is what was throwing an error when the server started.
    
    Another issue could be the ES6 arrow function.  I changed it back to ES2015
    syntax and the server started and rendered the form on the client side
    without an error.
    
    I need to test this further to isolate the bug for future projects.

--- POST-method/server.js
+++ POST-method/server.js
@@ -9,22 +9,29 @@ function handleRequest(req, res) {
     let path = req.url
     switch (path) {
         case '/':
-            return displayWelcomePage(path, req, res)
+            return displayWelcomePage(req, res)
         case '/thanks':
             return displayThankYouPage(path, req, res)
     }
 }
 
-const displayWelcomePage = (url, req, res) => {
+function displayWelcomePage(req, res) {
     fs.readFile(__dirname + '/index.html', function (err, data) {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        })
-        res.end(data)
+        if (err) {
+            res.writeHead(500, {
+                'Content-Type': 'text/html'
+            })
+            res.end('<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1><html>')
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.end(data)
+        }
     })
 }
 
-const displayThankYouPage = (url, req, res) => {
+const displayThankYouPage = (req, res) => {
     let requestData = ''
     req.on('data', (data) => {
         requestData += data;
